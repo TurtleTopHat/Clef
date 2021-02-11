2d5f8c978404105929ee861ca698327c8b4c94c7
Jacob Duggan <jacobduggan19th@gmail.com>
2021-02-11 20:15:55 +0000

    Buttons now functioning, pyramids display chords

--- Clef Final/public/javascript/script.js
+++ Clef Final/public/javascript/script.js
@@ -1,34 +1,96 @@
-"use strict";
+var piano_vis = false;
 
-function midi_test(){
-  if (navigator.requestMIDIAccess) {
-    console.log('This browser supports WebMIDI!');
-} else {
-    console.log('WebMIDI is not supported in this browser.');
+function display_piano(){
+  if(!piano_vis){
+    document.getElementById("piano").style.visibility = "visible";
+    piano_vis = true;
+  } else {
+    document.getElementById("piano").style.visibility = "hidden";
+    piano_vis = false;
+  }
 }
+
+var note_selected = "default";
+function note_select(n){
+  note_selected = n;
+  switch(note_selected){
+    case "A":
+    document.getElementById("b").style.visibility = "visible";
+    document.getElementById("#").style.visibility = "visible";
+    break;
+    case "B":
+    document.getElementById("b").style.visibility = "visible";
+    document.getElementById("#").style.visibility = "hidden";
+    break;
+    case "C":
+    document.getElementById("b").style.visibility = "visible";
+    document.getElementById("#").style.visibility = "visible";
+    break;
+    case "D":
+    document.getElementById("b").style.visibility = "visible";
+    document.getElementById("#").style.visibility = "visible";
+    break;
+    case "E":
+    document.getElementById("b").style.visibility = "visible";
+    document.getElementById("#").style.visibility = "hidden";
+    break;
+    case "F":
+    document.getElementById("b").style.visibility = "visible";
+    document.getElementById("#").style.visibility = "visible";
+    break;
+    case "G":
+    document.getElementById("b").style.visibility = "visible";
+    document.getElementById("#").style.visibility = "visible";
+    break;
+  }
+}
+
+var accidental = "natural";
+function sharp_or_flat(a){
+  if(a != null){
+    accidental = a;
+  } else{
+    accidental = "natural";
+  }
+}
+
+var tonality;
+function maj_or_min(t){
+  tonality = t;
 }
 
 function run(){
-  var answer = document.getElementById("note_val").value;
-  answer = note_to_num(answer);
-  construct_major(answer);
+    alert(note_selected + " " + accidental + " " + tonality);
+    var num = note_to_num(note_selected, accidental);
+
+    if(tonality == "maj"){
+      construct_major(num);
+    } else {
+      construct_minor(num);
+    }
 }
 
-function note_to_num(val){
+function note_to_num(note_name, accidental_name){
   var out = 0;
+  var val = note_name + accidental_name;
 
-  if(val == 'A') out = 1;
-  else if(val == 'A#') out = 2;
-  else if(val == 'B') out = 3;
-  else if(val == 'C') out = 4;
+  if(val == "Anatural") out = 1;
+  else if(val == "Bb") out = 2;
+  else if(val == "A#") out = 2;
+  else if(val == 'Bnatural') out = 3;
+  else if(val == 'Cnatural') out = 4;
   else if(val == 'C#') out = 5;
-  else if(val == 'D') out = 6;
+  else if(val == 'Db') out = 5;
+  else if(val == 'Dnatural') out = 6;
   else if(val == 'D#') out = 7;
-  else if(val == 'E') out = 8;
-  else if(val == 'F') out = 9;
+  else if(val == 'Eb') out = 7;
+  else if(val == 'Enatural') out = 8;
+  else if(val == 'Fnatural') out = 9;
   else if(val == 'F#') out = 10;
-  else if(val == 'G') out = 11;
+  else if(val == 'Gb') out = 10;
+  else if(val == 'Gnatural') out = 11;
   else if(val == 'G#') out = 12;
+  else if(val == 'Ab') out = 12;
   else {
     out = 0;
   }
@@ -97,15 +159,62 @@ function construct_major(val){
   }
 
   alert(scale_string);
-  construct_maj_chords(scale_array);
+  construct_chords(scale_array);
 }
 
-function construct_maj_chords(val){
-    document.getElementById('first').innerHTML = val[0] + val[2] + val[4];
-    document.getElementById('second').innerHTML = val[1] + val[3] + val[5];
-    document.getElementById('third').innerHTML = val[2] + val[4] + val[6];
-    document.getElementById('fourth').innerHTML = val[3] + val[5] + val[0];
-    document.getElementById('fifth').innerHTML = val[4] + val[6] + val[1];
-    document.getElementById('sixth').innerHTML = val[5] + val[0] + val[2];
-    document.getElementById('seventh').innerHTML = val[6] + val[1] + val[3];
+function construct_minor(val){
+  //WWHWWWH
+  var scale_string;
+  var scale_array = [7];
+  if(val == 0) alert("Error! Non valid input!");
+  else{
+    scale_array[0] = num_to_note(val);
+    scale_string = num_to_note(val);
+
+    val += 2;
+    val %= 12;
+    scale_array[1] = num_to_note(val);
+    scale_string += " " + num_to_note(val);
+
+    val += 1;
+    val %= 12;
+    scale_array[2] = num_to_note(val);
+    scale_string += " " + num_to_note(val);
+
+    val += 2;
+    val %= 12;
+    scale_array[3] = num_to_note(val);
+    scale_string += " " + num_to_note(val);
+
+    val += 2;
+    val %= 12;
+    scale_array[4] = num_to_note(val);
+    scale_string += " " + num_to_note(val);
+
+    val += 1;
+    val %= 12;
+    scale_array[5] = num_to_note(val);
+    scale_string += " " + num_to_note(val);
+
+    val += 2;
+    val %= 12;
+    scale_array[6] = num_to_note(val);
+    scale_string += " " + num_to_note(val);
+  }
+
+  alert(scale_string);
+  construct_chords(scale_array);
+}
+
+function construct_chords(val){
+
+    document.getElementById('I').innerHTML = val[0] + val[2] + val[4];
+    document.getElementById('V').innerHTML = val[4] + val[6] + val[1];
+    document.getElementById('IV').innerHTML = val[3] + val[5] + val[0];
+    document.getElementById('II').innerHTML = val[1] + val[3] + val[5];
+    document.getElementById('VI').innerHTML = val[5] + val[0] + val[2];
+    document.getElementById('III').innerHTML = val[2] + val[4] + val[6];
+
+    document.getElementById("b").style.visibility = "hidden";
+    document.getElementById("#").style.visibility = "hidden";
 }
