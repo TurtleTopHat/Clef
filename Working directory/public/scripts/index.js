3db544c84490497d6a4ff1be7fe2b48f6ad5b3bc
Conorc32 <72629448+Conorc32@users.noreply.github.com>
2021-03-06 17:10:06 +0000

    Update index.js

--- Working directory/public/scripts/index.js
+++ Working directory/public/scripts/index.js
@@ -32,29 +32,7 @@ const setupUI = (user) => {
   }
 };
 
-/**
-// setup guides
-const setupGuides = (data) => {
 
-  if (data.length) {
-    let html = '';
-    data.forEach(doc => {
-      const guide = doc.data();
-      const li = `
-        <li>
-          <div class="collapsible-header grey lighten-4"> ${guide.title} </div>
-          <div class="collapsible-body white"> ${guide.content} </div>
-        </li>
-      `;
-      html += li;
-    });
-    guideList.innerHTML = html
-  } else {
-    guideList.innerHTML = '<h5 class="center-align"></h5>';
-  }
-  
-
-};**/
 
 function doSomething()
 {
@@ -78,21 +56,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
